339c51c1d33a6b3283b833318a6dd94b21972f28
Niyaz <jniyaz@gmail.com>
2020-05-26 11:25:51 +0800

    fixed all functional logics

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,31 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { v4 as uuid } from "uuid";
+import { GlobalContext } from "../context/GlobalState";
 
 export const AddTransaction = () => {
   const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
+  const { addTransaction } = useContext(GlobalContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTransaction = {
+      id: uuid(),
+      text,
+      amount: +amount,
+    };
+    addTransaction(newTransaction);
+    setText("");
+    setAmount(0);
+  };
+
   return (
     <>
       <h3>Add New Transaction</h3>
-      <form id="form">
+      <form onSubmit={handleSubmit}>
         <div className="form-control">
-          <label htmlFor="text">Text</label>
+          <label htmlFor="text">Transaction Label</label>
           <input
             type="text"
             value={text}
             onChange={(e) => setText(e.target.value)}
             placeholder="Enter text..."
+            required
           />
         </div>
         <div className="form-control">
           <label htmlFor="amount">
-            Amount <br />
-            (negative - expense, positive - income)
+            Transaction Amount <br />
+            <small>
+              <i>(negative - expense, positive - income)</i>
+            </small>
           </label>
           <input
             type="number"
             value={amount}
             onChange={(e) => setAmount(e.target.value)}
             placeholder="Enter amount..."
+            required
           />
         </div>
         <button className="btn">Add transaction</button>
